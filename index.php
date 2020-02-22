b88ad4c54a2977007b047d6eec4fbbff9dd079f4
Nick Turitto <nick.turitto@gmail.com>
2020-02-21 19:21:53 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include header.php ?>
+
+<div class="row">
+    <div class="col-sm-3">
+        <p>we</p>
+    </div>
+    <div class="col-sm-3">
+        <p>doing</p>
+    </div>
+    <div class="col-sm-3">
+        <p>this????</p>
+    </div>
+</div>
+
+
+
+<?php include footer.php ?>
